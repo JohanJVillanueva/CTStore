6b4f959dc9a356eb01acc541fa4b2aa5239dd06a
JohanJVillanueva <johanjvillanueva@iskolarngbayan.edu.ph>
2023-10-31 17:17:33 +0800

    added randomizer hero photo

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+
+window.onload = choosePic;
+
+var myPix = new Array("Hero/tk68.jpg","Hero/_MG_1371.jpg","Hero/_MG_3759.jpg");
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * myPix.length);
+     document.getElementById("myPicture").src = myPix[randomNum];
+    }
+
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+      }
